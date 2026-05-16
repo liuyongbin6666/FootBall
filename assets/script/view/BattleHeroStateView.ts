f5c181400eec20fb5fa0081c0c80c57aa1434ad1
Liu Yongbin <44885895+liuyongbin6666@users.noreply.github.com>
2026-05-16 17:43:09 +0800

    更新

--- assets/script/view/BattleHeroStateView.ts
+++ assets/script/view/BattleHeroStateView.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Label, Button, find, Color, Sprite } from 'cc';
+import { _decorator, Component, Node, Label, Button, find, Color, Sprite, Layout, Prefab, instantiate } from 'cc';
 import { Layer } from '../manager/Layer';
 const { ccclass, property } = _decorator;
 
@@ -26,15 +26,6 @@ interface HeroBaseData {
 
 @ccclass('BattleHeroStateView')
 export class BattleHeroStateView extends Component {
-    
-    @property(Node)//标签按钮容器
-    private lay_tab: Node = null!;//标签按钮容器
-    // 标签按钮数组
-    private tabButtons: Node[] = [];
-    
-    // 当前选中的英雄索引
-    private currentSelectHeroIndex: number = 0;
-    
     // 上阵英雄数据列表（模拟数据，实际有2个英雄）
     private heroDataList: HeroBaseData[] = [
         { 
@@ -51,7 +42,23 @@ export class BattleHeroStateView extends Component {
         }
     ];
 
-    // 信息面板组件
+    // 英雄列表布局组件
+    private lay_heroList: Layout | null = null;
+    @property(Prefab)
+    private heroListItem: Prefab = null;
+
+    // 标签按钮布局组件
+    private lay_tab: Layout | null = null;
+    @property(Prefab)
+    private tabItem: Prefab = null;
+    // 标签按钮数组
+    private tabButtons: Node[] = [];
+    // 当前选中的英雄索引
+    private currentSelectHeroIndex: number = 0;
+    
+    // 窗口节点
+    private node_window: Node | null = null;
+    // 窗口面板组件
     private btn_exit: Button | null = null;//退出按钮
 
     private img_avatarBg: Sprite | null = null;//头像框
@@ -63,40 +70,13 @@ export class BattleHeroStateView extends Component {
 
     private btn_remove: Button | null = null;//移除按钮
 
-    //标签按钮
-    private btn_1: Button | null = null;//btn_1 序列1的英雄的按钮
-    private lab_1: Label | null = null;//lab_1 序列1的英雄名
-
-    private btn_2: Button | null = null;//btn_2 序列2的英雄的按钮
-    private lab_2: Label | null = null;//lab_2 序列2的英雄名
-
-    private btn_3: Button | null = null;//btn_3 序列3的英雄的按钮
-    private lab_3: Label | null = null;//lab_3 序列3的英雄名
-
-    private btn_4: Button | null = null;//btn_4 序列4的英雄的按钮
-    private lab_4: Label | null = null;//lab_4 序列4的英雄名
-
-    private btn_5: Button | null = null;//btn_5 序列5的英雄的按钮
-    private lab_5: Label | null = null;//lab_5 序列5的英雄名
-
-    private btn_6: Button | null = null;//btn_6 序列6的英雄的按钮
-    private lab_6: Label | null = null;//lab_6 序列6的英雄名
-
-    private btn_7: Button | null = null;//btn_7 序列7的英雄的按钮
-    private lab_7: Label | null = null;//lab_7 序列7的英雄名
-
-    private btn_heroView1:Button;
-    private btn_heroView2:Button;
-    private node_heroState:Node;
-
     protected onLoad(): void {
         this._initObject();
-        this._initTabs();
         this._onEvent();
     }
 
     start() {
-        // this.node_heroState.active = false;
+        this.node_window.active = false;
         this.refreshHeroInfo(0);
     }
 
@@ -105,70 +85,60 @@ export class BattleHeroStateView extends Component {
     }
 
     private _initObject(): void {
-        this.btn_exit = find('btn_exit', this.node)?.getComponent(Button) || null;
-
-        this.img_avatarBg = find('lay_window/lay_main/lay_heroInfo/img_avatarBg', this.node)?.getComponent(Sprite) || null;
-        this.img_avatar = find('lay_window/lay_main/lay_heroInfo/img_avatarBg/img_avatar', this.node)?.getComponent(Sprite) || null;
-        this.lab_heroName = find('lay_window/lay_main/lay_heroInfo/img_avatarBg/lab_heroName', this.node)?.getComponent(Label) || null;
+        this.lay_heroList = find('lay_heroList', this.node).getComponent(Layout);
+        this.lay_tab = find('lay_tab', this.node).getComponent(Layout);
+        this.updateHeros();
         
-        this.lab_harm = find('lay_window/lay_main/lay_heroInfo/base/harm/lab_harm', this.node)?.getComponent(Label) || null;
-        this.lab_maxHP = find('lay_window/lay_main/lay_heroInfo/base/maxHP/lab_maxHP', this.node)?.getComponent(Label) || null;
-        this.lab_heroIntroduce = find('lay_window/lay_main/lay_heroInfo/img_heroIntroduce/lab_heroIntroduce', this.node)?.getComponent(Label) || null;
+        this.node_window = find('node_window', this.node);
+        this.btn_exit = find('node_window/btn_exit', this.node)?.getComponent(Button) || null;
+
+        this.img_avatarBg = find('node_window/window/heroInfo/img_avatarBg', this.node)?.getComponent(Sprite) || null;
+        this.img_avatar = find('node_window/window/heroInfo/img_avatarBg/img_avatar', this.node)?.getComponent(Sprite) || null;
+        this.lab_heroName = find('node_window/window/heroInfo/img_avatarBg/lab_heroName', this.node)?.getComponent(Label) || null;
         
-        this.btn_remove = find('lay_window/lay_main/lay_operate/btn_remove', this.node)?.getComponent(Button) || null;
-
-        this.btn_1 = find('lay_window/lay_tab/btn_1', this.node)?.getComponent(Button) || null;
-        this.lab_1 = find('lay_window/lay_tab/btn_1/lab_1', this.node)?.getComponent(Label) || null;
-        this.btn_2 = find('lay_window/lay_tab/btn_2', this.node)?.getComponent(Button) || null;
-        this.lab_2 = find('lay_window/lay_tab/btn_2/lab_2', this.node)?.getComponent(Label) || null;
-        this.btn_3 = find('lay_window/lay_tab/btn_3', this.node)?.getComponent(Button) || null;
-        this.lab_3 = find('lay_window/lay_tab/btn_3/lab_3', this.node)?.getComponent(Label) || null;
-        this.btn_4 = find('lay_window/lay_tab/btn_4', this.node)?.getComponent(Button) || null;
-        this.lab_4 = find('lay_window/lay_tab/btn_4/lab_4', this.node)?.getComponent(Label) || null;
-        this.btn_5 = find('lay_window/lay_tab/btn_5', this.node)?.getComponent(Button) || null;
-        this.lab_5 = find('lay_window/lay_tab/btn_5/lab_5', this.node)?.getComponent(Label) || null;
-        this.btn_6 = find('lay_window/lay_tab/btn_6', this.node)?.getComponent(Button) || null;
-        this.lab_6 = find('lay_window/lay_tab/btn_6/lab_6', this.node)?.getComponent(Label) || null;
-        this.btn_7 = find('lay_window/lay_tab/btn_7', this.node)?.getComponent(Button) || null;
-        this.lab_7 = find('lay_window/lay_tab/btn_7/lab_7', this.node)?.getComponent(Label) || null;
-
-        this.btn_heroView1 = find('btn_heroView1', this.node).getComponent(Button);
-        this.btn_heroView2 = find('btn_heroView2', this.node).getComponent(Button);
-        this.node_heroState = find('node_heroState', this.node);
+        this.lab_harm = find('node_window/window/heroInfo/base/harm/lab_harm', this.node)?.getComponent(Label) || null;
+        this.lab_maxHP = find('node_window/window/heroInfo/base/maxHP/lab_maxHP', this.node)?.getComponent(Label) || null;
+        this.lab_heroIntroduce = find('node_window/window/heroInfo/heroIntroduce/lab_heroIntroduce', this.node)?.getComponent(Label) || null;
+        //todo 技能
+        this.btn_remove = find('node_window/window/operate/btn_remove', this.node)?.getComponent(Button) || null;
     }
 
-private _initTabs(): void {
-    if (!this.lay_tab) return;       // 防护性检查
-    this.tabButtons = [];             // 清空旧的按钮数组
     
-    for (let i = 0; i < 7; i++) {    // 遍历最多7个标签位
-        const tabNode = find(`lay_window/lay_tab/btn_${i + 1}`, this.lay_tab);  // 查找节点
-        if (tabNode) {
-            this.tabButtons.push(tabNode);  // 加入数组
-            
-            if (i < this.heroDataList.length) {  // 有英雄数据
-                tabNode.active = true;           // 显示按钮
-                const heroData = this.heroDataList[i];
-                const labName = find(`lab_${i + 1}`, tabNode)?.getComponent(Label);//查找英雄名标签
-                if (labName) {
-                    labName.string = heroData.heroName;  // 设置英雄名
-                }
-            } else {
-                tabNode.active = false;  // 无数据则隐藏按钮
-            }
+    private updateHeros(): void {
+        //清空列表
+        this.lay_heroList.node.removeAllChildren();
+        this.lay_tab.node.removeAllChildren();
+
+        for (let i = 0; i < this.heroDataList.length; i++) {
+            this.createHeroListItem(i);
+            this.createTabItem(i);
         }
     }
-    
-    this.updateTabSelectState();  // 更新选中状态
-}
+
+    private createHeroListItem(index: number): void {
+        let item = instantiate(this.heroListItem);
+        //todo 设置英雄信息
+        this.lay_heroList.node.addChild(item);
+    }
+
+    private createTabItem(index: number): void {
+        let item = instantiate(this.tabItem);
+        const heroData = this.heroDataList[index];
+        const labName = find('lab_heroName', item)?.getComponent(Label);
+        if (labName) {
+            labName.string = heroData.heroName;
+        }
+        item.active = true;
+        this.tabButtons.push(item);
+        this.lay_tab.node.addChild(item);
+    }
 
     private _onEvent(): void {
-        this.btn_heroView1.node.on(Node.EventType.TOUCH_END, this.openHeroView, this);
-        this.btn_heroView2.node.on(Node.EventType.TOUCH_END, this.openHeroView, this);
+        //this.btn_heroView2.node.on(Node.EventType.TOUCH_END, this.openHeroView, this);
         // 退出按钮点击事件
-        if (this.btn_exit) {
-            this.btn_exit.node.on(Node.EventType.TOUCH_END, this.closeView, this);
-        }
+        this.btn_exit.node.on(Node.EventType.TOUCH_END, this.closeView, this);
+        // 移除按钮点击事件
+        //this.btn_remove.node.on(Node.EventType.TOUCH_END, this.removeHero, this);
 
         for (let i = 0; i < this.tabButtons.length; i++) {
             if (i < this.heroDataList.length) {
@@ -178,13 +148,16 @@ private _initTabs(): void {
             }
         }
     }
+
     openHeroView()
     {
-        if(this.node_heroState.active)
+        if(this.node_window.active)
         {
-            this.node_heroState.active = false;
+            this.node_window.active = false;
+            this.lay_tab.node.active = false;
         }else{
-            this.node_heroState.active = true;
+            this.node_window.active = true;
+            this.lay_tab.node.active = true;
         }
     }
 
